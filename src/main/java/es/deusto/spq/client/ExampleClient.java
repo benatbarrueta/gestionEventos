3d83490533a0d2424611614fd99ee886a6c1ae29
Beñat Barrueta <benat.barrueta@asesormasmovil.es>
2024-03-25 13:21:26 +0100

    Añadir metodo crear usuario a clase exampleClient

--- src/main/java/es/deusto/spq/client/ExampleClient.java
+++ src/main/java/es/deusto/spq/client/ExampleClient.java
@@ -1,5 +1,9 @@
 package es.deusto.spq.client;
 
+import java.util.Date;
+
+import javax.jdo.annotations.Persistent;
+import javax.jdo.annotations.PrimaryKey;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.Entity;
@@ -9,9 +13,9 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 
-import es.deusto.spq.pojo.DirectMessage;
-import es.deusto.spq.pojo.MessageData;
-import es.deusto.spq.pojo.UserData;
+import es.deusto.spq.server.jdo.Usuario;
+import es.deusto.spq.server.jdo.Evento;
+import es.deusto.spq.server.jdo.Entrada;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -32,42 +36,17 @@ public class ExampleClient {
 		webTarget = client.target(String.format("http://%s:%s/rest/resource", hostname, port));
 	}
 
-	public void registerUser(String login, String password) {
+	public void registerUser(String nombre, String apellidos, String nombreUsuario, String contrasenya, String email, String direccion, String telefono, String rol, Date fechaNacimiento, String dni) {
 		WebTarget registerUserWebTarget = webTarget.path("register");
 		Invocation.Builder invocationBuilder = registerUserWebTarget.request(MediaType.APPLICATION_JSON);
+
+		Usuario userData = new Usuario(nombre, apellidos, nombreUsuario, contrasenya, email, direccion, telefono, rol, fechaNacimiento, dni);
 		
-		UserData userData = new UserData();
-		userData.setLogin(login);
-		userData.setPassword(password);
 		Response response = invocationBuilder.post(Entity.entity(userData, MediaType.APPLICATION_JSON));
 		if (response.getStatus() != Status.OK.getStatusCode()) {
 			logger.error("Error connecting with the server. Code: {}", response.getStatus());
 		} else {
 			logger.info("User correctly registered");
-		}
-	}
-
-	public void sayMessage(String login, String password, String message) {
-		WebTarget sayHelloWebTarget = webTarget.path("sayMessage");
-		Invocation.Builder invocationBuilder = sayHelloWebTarget.request(MediaType.APPLICATION_JSON);
-
-		DirectMessage directMessage = new DirectMessage();
-		UserData userData = new UserData();
-		userData.setLogin(login);
-		userData.setPassword(password);
-
-		directMessage.setUserData(userData);
-
-		MessageData messageData = new MessageData();
-		messageData.setMessage(message);
-		directMessage.setMessageData(messageData);
-
-		Response response = invocationBuilder.post(Entity.entity(directMessage, MediaType.APPLICATION_JSON));
-		if (response.getStatus() != Status.OK.getStatusCode()) {
-			logger.error("Error connecting with the server. Code: {}",response.getStatus());
-		} else {
-			String responseMessage = response.readEntity(String.class);
-			logger.info("* Message coming from the server: '{}'", responseMessage);
 		}
 	}
 
@@ -81,7 +60,6 @@ public class ExampleClient {
 		String port = args[1];
 
 		ExampleClient exampleClient = new ExampleClient(hostname, port);
-		exampleClient.registerUser(USER, PASSWORD);
-		exampleClient.sayMessage(USER, PASSWORD, "This is a test!...");
+		exampleClient.registerUser("David", "Pina", "dipina", "dipina", "a", "a", "a", "a", new Date(System.currentTimeMillis()), "12345678A");
 	}
 }
