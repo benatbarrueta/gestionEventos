93c163f28b3a9110c9daf960d19618cfe301b857
yeraychacon <115021795+yeraychacon@users.noreply.github.com>
2024-03-27 00:08:59 +0100

    menubar update

--- src/main/java/es/deusto/spq/client/gui/MainWindow.java
+++ src/main/java/es/deusto/spq/client/gui/MainWindow.java
@@ -1,6 +1,7 @@
 package es.deusto.spq.client.gui;
 
-import javax.swing.JFrame;
+import javax.swing.*;
+import javax.swing.JMenuBar;
 
 import es.deusto.spq.client.Main;
 
@@ -14,23 +15,23 @@ public class MainWindow extends JFrame{
         this.setSize(1000, 800);
         this.setLocationRelativeTo(null);
         this.setTitle("Main Window");
-        this.setLayout(new FLowLayout());
+       
+        
 
-        JMenuBar menuBar = new JMenuBar
+        JMenuBar menuBar = new JMenuBar();
         JMenu menu = new JMenu("Menu");
-        JMenuItem menuItem = new JMenuItem("Cerrar Sesion");
+        JMenuItem logOutItem = new JMenuItem("Cerrar Sesion");
         
         this.setJMenuBar(menuBar);
-        menorBar.add(menu);
-        menu.add(menuItem);
+        menuBar.add(menu);
+        menu.add(logOutItem);
         
-        menuItem.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                main.logout();
-                this.setVisible(false);
-                main.loginWindow.setVisible(true);
-            }
+
+        logOutItem.addActionListener(e -> {
+            main.logout();
+            this.dispose();
+            Main.loginWindow.setVisible(true);
         });
+        
     }
 }
