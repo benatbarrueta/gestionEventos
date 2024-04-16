60885b7e8cec6383d0ee69b414bef57acc5fae82
Beñat Barrueta <115789920+benatbarrueta@users.noreply.github.com>
2024-04-16 13:55:35 +0200

    añadir test de clase entrada
    Co-Authored-By: Gorkaaa <126794097+Gorkaaa@users.noreply.github.com>
    Co-Authored-By: morales44 <115790097+morales44@users.noreply.github.com>
    Co-Authored-By: yeraychacon <115021795+yeraychacon@users.noreply.github.com>

--- src/test/java/es/deusto/spq/server/jdo/EntradaTest.java
+++ src/test/java/es/deusto/spq/server/jdo/EntradaTest.java
@@ -1,12 +1,68 @@
 package es.deusto.spq.server.jdo;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Before;
+import org.junit.Test;
 
 public class EntradaTest {
     private Entrada entrada;
 
     @Before
     public void setUp() {
-        entrada = new Entrada(null, null, 0, null);
+        entrada = new Entrada(new Usuario(), new Evento(), 0, SectoresEvento.PISTA);
+    }
+
+    @Test
+    public void testGetUsuario() {
+        assertEquals(new Usuario().getDni(), entrada.getUsuario().getDni());
+    }
+
+    @Test
+    public void testSetUsuario() {
+        entrada.getUsuario().setDni("2");;
+        assertEquals("2", entrada.getUsuario().getDni());
+    }
+
+    @Test
+    public void testGetEvento() {
+        assertEquals(new Evento().getNombre(), entrada.getEvento().getNombre());
+    }
+
+    @Test
+    public void testSetEvento() {
+        entrada.getEvento().setNombre("2");
+        assertEquals("2", entrada.getEvento().getNombre());
+    }
+
+    @Test
+    public void testGetPrecio() {
+        assertEquals(0, entrada.getPrecio());
+    }
+
+    @Test
+    public void testSetPrecio() {
+        entrada.setPrecio(2);
+        assertEquals(2, entrada.getPrecio());
+    }
+
+    @Test
+    public void testGetSector() {
+        assertEquals(SectoresEvento.PISTA, entrada.getSector());
+    }
+
+    @Test
+    public void testSetSector() {
+        entrada.setSector(SectoresEvento.PISTA);
+    }
+
+    @Test
+    public void testToString() {
+        assertEquals("Entrada 0 usuario: , evento: null", entrada.toString());
+    }
+
+    @Test
+    public void testToString2() {
+        assertEquals("Entrada 0 usuario: , evento: null", entrada.toString());
     }
 }
