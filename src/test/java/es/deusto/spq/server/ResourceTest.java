179c208b9cb91cd2cabb55b8f8b0ce42c67956d3
Beñat Barrueta <115789920+benatbarrueta@users.noreply.github.com>
2024-04-16 14:11:32 +0200

    añadir test de la parte del usuario
    Co-Authored-By: Gorkaaa <126794097+Gorkaaa@users.noreply.github.com>
    Co-Authored-By: morales44 <115790097+morales44@users.noreply.github.com>
    Co-Authored-By: yeraychacon <115021795+yeraychacon@users.noreply.github.com>

--- src/test/java/es/deusto/spq/server/ResourceTest.java
+++ src/test/java/es/deusto/spq/server/ResourceTest.java
@@ -1,5 +1,70 @@
 package es.deusto.spq.server;
 
-public class ResourceTest {
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
 
+import java.util.Date;
+
+import javax.ws.rs.core.Response;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+
+import es.deusto.spq.server.jdo.Entrada;
+import es.deusto.spq.server.jdo.Evento;
+import es.deusto.spq.server.jdo.TipoUsuario;
+import es.deusto.spq.server.jdo.Usuario;
+
+public class ResourceTest {
+    Usuario u;
+
+    @Mock
+    private Usuario usuario;
+
+    @Mock
+    private Evento evento;
+
+    @Mock
+    private Entrada entrada;
+
+    private Resource resource;
+
+    @Mock
+    private Response response;
+
+    Date fecha = new Date(System.currentTimeMillis());
+
+    @Before
+    public void setUp() {
+        usuario = mock(Usuario.class);
+        evento = mock(Evento.class);
+        entrada = mock(Entrada.class);
+        response = mock(Response.class);
+
+        resource = new Resource();
+
+        u = new Usuario("test","test", "test","test","test","test","test",TipoUsuario.CLIENTE,fecha, "test");
+    }
+
+    @Test
+    public void testLoginUserFails() throws Exception{
+        assertEquals(Response.Status.UNAUTHORIZED.getStatusCode(), resource.loginUser(usuario).getStatus());
+    }
+
+    @Test
+    public void testRegisterUser() throws Exception{
+        resource.registerUser(u);
+        assertEquals(Response.Status.OK.getStatusCode(), resource.loginUser(u).getStatus());
+    }
+
+    @Test
+    public void testLogout() throws Exception{
+        assertEquals(Response.Status.OK.getStatusCode(), resource.logout().getStatus());
+    }
+
+    @Test
+    public void testEliminarCuenta() throws Exception{
+        assertEquals(Response.Status.OK.getStatusCode(), resource.eliminarCuenta(u.getDni()).getStatus());
+    }
 }
