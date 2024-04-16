7ed8ada14fa3a34e31952bd782fcbb3cb1a9bd99
Beñat Barrueta <benat.barrueta@opendeusto.es>
2024-04-16 10:58:49 +0200

    empezar con test
    Co-Authored-By: Gorkaaa <126794097+Gorkaaa@users.noreply.github.com>
    Co-Authored-By: morales44 <115790097+morales44@users.noreply.github.com>
    Co-Authored-By: yeraychacon <115021795+yeraychacon@users.noreply.github.com>

---
+++ src/test/server/jdo/UsuarioTest.java
@@ -0,0 +1,112 @@
+import es.deusto.spq.server.jdo.TipoUsuario;
+import es.deusto.spq.server.jdo.Usuario;
+
+public class UsuarioTest {
+    
+    private Usuario usuario;
+
+    @Before
+    public void setUp() {
+        usuario = new Usuario("test", "test", "test", "test", "test", "test", "test", TipoUsuario.CLIENTE, new Date(System.currentTimeMillis()), "1");
+    }
+
+    @Test
+    public void testGetNombre() {
+        assertEquals("test", usuario.getNombre());
+    }
+
+    @Test
+    public void testSetNombre() {
+        usuario.setNombre("test2");
+        assertEquals("test2", usuario.getNombre());
+    }
+
+    @Test
+    public void testGetApellidos() {
+        assertEquals("test", usuario.getApellidos());
+    }
+
+    @Test
+    public void testSetApellidos() {
+        usuario.setApellidos("test2");
+        assertEquals("test2", usuario.getApellidos());
+    }
+
+    @Test
+    public void testGetEmail() {
+        assertEquals("test", usuario.getEmail());
+    }
+
+    @Test
+    public void testSetEmail() {
+        usuario.setEmail("test2");
+        assertEquals("test2", usuario.getEmail());
+    }
+
+    @Test
+    public void testGetContrasenya() {
+        assertEquals("test", usuario.getContrasenya());
+    }
+
+    @Test
+    public void testSetContrasenya() {
+        usuario.setContrasenya("test2");
+        assertEquals("test2", usuario.getContrasenya());
+    }
+
+    @Test
+    public void testGetDireccion() {
+        assertEquals("test", usuario.getDireccion());
+    }
+
+    @Test
+    public void testSetDireccion() {
+        usuario.setDireccion("test2");
+        assertEquals("test2", usuario.getDireccion());
+    }
+
+    @Test
+    public void testGetTelefono() {
+        assertEquals("test", usuario.getTelefono());
+    }
+
+    @Test
+    public void testSetTelefono() {
+        usuario.setTelefono("test2");
+        assertEquals("test2", usuario.getTelefono());
+    }
+
+    @Test
+    public void testGetTipoUsuario() {
+        assertEquals(TipoUsuario.CLIENTE, usuario.getTipoUsuario());
+    }
+
+    @Test
+    public void testSetTipoUsuario() {
+        usuario.setTipoUsuario(TipoUsuario.ADMINISTRADOR);
+        assertEquals(TipoUsuario.ADMINISTRADOR, usuario.getTipoUsuario());
+    }
+
+    @Test
+    public void testGetFechaNacimiento() {
+        assertEquals(new Date(System.currentTimeMillis()), usuario.getFechaNacimiento());
+    }
+
+    @Test
+    public void testSetFechaNacimiento() {
+        Date date = new Date(System.currentTimeMillis());
+        usuario.setFechaNacimiento(date);
+        assertEquals(date, usuario.getFechaNacimiento());
+    }
+
+    @Test
+    public void testGetDni() {
+        assertEquals("1", usuario.getDni());
+    }
+
+    @Test
+    public void testSetDni() {
+        usuario.setDni("2");
+        assertEquals("2", usuario.getDni());
+    }
+}
